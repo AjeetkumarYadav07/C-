d4466dfe8d8a595f462ff269d6ee9b81fa48084c
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-12-02 16:31:23 +0530

    add  more files

---
+++ first program/operators.cpp
@@ -0,0 +1,29 @@
+# include <iostream> 
+# include <string>
+
+using namespace std;
+
+int main (){
+   
+    
+     int a , b ;
+
+    cout <<"enter the first number " <<endl;
+
+    cin>> a;
+
+    cout <<"enter the Second number " <<endl;
+
+    cin>> b; 
+
+
+ cout << "Print all operators" << endl;
+   
+   cout << " A + B = " << a +b << endl;
+   cout << "A - B   =" << a - b <<endl;
+   cout << "A * B =" << a  *b <<endl ;
+   cout << "A / B=" << (float) a /b << endl;
+
+   return 0;
+ 
+}
