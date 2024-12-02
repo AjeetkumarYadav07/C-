00d5c0433f53b985ff8de53e660a1e5736e69923
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-12-02 15:35:09 +0530

    first files

---
+++ first program/datatype.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+ int main (){
+    //Types of datatypes //
+
+    int myNum = 5;  //Integer (whole number)
+    float myFloatNum = 5.99;  //Floatfing point numer
+    double myDoubleNum = 9.98;
+    char myLetter = 'D';
+    bool myBoolean = true;
+    string myString = "helo";  //String
+
+
+
+      ///  For (Print the values)
+
+      cout <<myNum <<"\n";
+      cout <<myString;
+
+      return 0 ;
+      
+ }
---
+++ first program/userinput.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+ int main (){
+    int a ,b ;
+    cout<< "Enter yiur number" <<endl;
+    cin>>  a;
+    cout<< "enter  second number" <<endl;
+    cin>> b;
+    
+    cout<< "the sum of a+b is = " << a +b;
+
+
+
+
+    return 0;
+ }
