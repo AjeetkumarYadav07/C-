5fa77123e525c2c34cc042196bf0b2dbd8492a76
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-12-04 15:55:30 +0530

    add some solution

--- first program/Array.cpp
+++ first program/Array.cpp
@@ -14,18 +14,40 @@
 //     return 0;
 //  }
 
- int main (){
+//  int main (){
     
-    int num [7] = {1 ,9, 2, 5,6,3, 7  };
-    int lastchild = num[0] ;
-      for ( int i = 0; i<7; i++){
-        if (num[i ] > lastchild)
-        {
-          lastchild = num[i]  ;
-        }
+//     int num [7] = {1 ,9, 2, 5,6,3, 7  };
+//     int lastchild = num[0] ;
+//       for ( int i = 0; i<7; i++){
+//         if (num[i ] > lastchild)
+//         {
+//           lastchild = num[i]  ;
+//         }
         
-      }
+//       }
 
-      cout << " the max number is " << lastchild << endl;
-    return 0;
- }
+//       cout << " the max number is " << lastchild << endl;
+//     return 0;
+//  }
+// int sumFun ( int num1 , int num2){
+//      return num1+ num2 ;
+// }
+
+// int main (){
+//   int numb [2]{  2 ,4};
+
+//   cout<< " the sum of two number is " << sumFun( numb[0] , numb[1]) <<endl;
+//   return 0;
+// }
+
+  
+  
+  int main (){
+      int arr [5] { 5 ,7 ,3,5,6,} ;
+       int input ;
+       cin >> input ;
+       
+       cout << arr[ input];
+
+      return 0 ;
+  }
