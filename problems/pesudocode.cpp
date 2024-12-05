c8c183f80e1a70175e2c3c868de0d8f22a6cf756
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-12-05 23:55:41 +0530

    add new question

--- problems/pesudocode.cpp
+++ problems/pesudocode.cpp
@@ -7,18 +7,33 @@ using namespace std ;
      return p*i*t/100;
 
  }
-int  main (){
-   int a , b ,c ;
+// int  main (){
+//    int a , b ,c ;
 
-   cout<<"Enter the Principal Amount"<<endl;
-   cin >> a ;
-   cout<<"Enter the Intrest Amount" <<endl;
-    cin >> b ;
-   cout<<"Enter the Time Amount" <<endl;
-    cin >> c;
+//    cout<<"Enter the Principal Amount"<<endl;
+//    cin >> a ;
+//    cout<<"Enter the Intrest Amount" <<endl;
+//     cin >> b ;
+//    cout<<"Enter the Time Amount" <<endl;
+//     cin >> c;
 
-    cout << "The total intrest is = "<< inputFun( a , b , c) <<endl;
+//     cout << "The total intrest is = "<< inputFun( a , b , c) <<endl;
 
-    return 0;
+//     return 0;
 
+// }
+
+int maxFun ( int num1 , int num2){
+    return num1< num2 ;
+}
+int main (){
+    int numb , numb2 ;
+    cout <<"Enter the first number";
+    cin>> numb ;
+
+
+    cout <<"Enter the first number";
+    cin>> numb2 ;
+   \
+  return 0;
 }
