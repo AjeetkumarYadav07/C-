bfbb06f85c945bfac35cfd55e726b34869f129b2
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-12-09 21:19:07 +0530

    add new files

---
+++ first program/try.cpp
@@ -0,0 +1,79 @@
+# include <iostream>
+# include <string>
+   using namespace std ;
+  
+   
+// int main (){
+//      string var = "hellow coder";
+//      cout << "the length" << var.length() ;
+
+//      string code = "hello codeer" "\n";
+//      code [0] = 'i';
+//      cout << code ;
+   
+//    int   i = 4 ;
+//      if ( i < 10){
+//         cout << "Heloo im if stament";
+//      }
+//   int num = 3 ;
+
+//   if( num %2 == 0){
+//     cout << "is good boy";
+//   }
+//    else {
+//     cout << " is bad boy" "\n";
+//    }
+
+//    int number = 88;
+
+//    if ( number >= 90  &&  number<= 100){
+//     cout << "A Grade";
+//    }
+//    else if (number >= 70 && number < 90){
+//     cout << "B Grade";
+//    }
+//    else {
+//     cout << "Fail sala" "\n";
+//    }
+//   int numb = 40 ;
+//    cout << ( numb %2 == 0 ? "Even" : "Odd") ;
+// }
+
+int main (){
+//    int n = 20;
+//    int i = 1 ;
+//   while( i <= n){
+//     cout<< i << " ";
+//     i++ ;
+  
+
+//     int sum = 0 ;
+//     int n = 5 ;
+
+//   for( int i = 1 ; i <=n ; i++){
+//      sum+= i;
+//   }
+//  cout << sum ;
+
+
+// int oddsum = 0 ;
+// int n = 5;
+// for(int i=1 ; i<=n ; i+=2){
+//     oddsum+=i;
+    
+// }
+// cout << oddsum ;
+int n = 15;
+
+
+  for (int i= 1 ; i<=n ; i++) {
+     if(n%i == !0){
+        cout<< i ;     }
+  }
+
+
+cout << "elod f";
+    return 0;
+
+    
+}
--- problems/pesudocode.cpp
+++ problems/pesudocode.cpp
@@ -3,10 +3,10 @@
 
 using namespace std ;
 
- int inputFun ( int p , int i , int t){
-     return p*i*t/100;
+//  int inputFun ( int p , int i , int t){
+//      return p*i*t/100;
 
- }
+//  }
 // int  main (){
 //    int a , b ,c ;
 
@@ -23,17 +23,73 @@ using namespace std ;
 
 // }
 
-int maxFun ( int num1 , int num2){
-    return num1< num2 ;
-}
-int main (){
-    int numb , numb2 ;
-    cout <<"Enter the first number";
-    cin>> numb ;
+// int maxFun ( int num1 , int num2){
+//     return num1< num2 ;
+// }
 
 
-    cout <<"Enter the first number";
-    cin>> numb2 ;
-   \
-  return 0;
+  /// <<***   Function for max number *** >>
+// int main (){
+//     int num1, num2 ;
+//     cout <<"Enter the first number";
+//     cin>> num1 ;
+
+
+//     cout <<"Enter the first number";
+//     cin>> num2 ;
+//    if ( num1> num2)
+//    {
+//      cout << num1 << " is the max number";
+//    }
+//    else {
+//     cout << num2 << "is the max number" ;
+//    }
+     
+//   return 0;
+// }
+
+/// factor of n number 
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    int i;
+    int factorial = 1;
+
+    // Prompt user for input
+    cout << "Enter the number: " << endl;
+    cin >> num;
+
+    // Check for negative input
+    if (num < 0) {
+        cout << "No factorial for negative numbers!" << endl;
+        return 0; // Exit the program
+    }
+
+    // Initialize i to 1
+    i = 1;
+
+    cout << "Factorial Calculation: ";
+
+    // Use a while loop for factorial calculation
+    while (i <= num) {
+        factorial *= i; // Multiply the current factorial value
+        cout << i; // Print the current number
+        if (i < num) {
+            cout << " x "; // Print multiplication sign for intermediate steps
+        }
+        i++;
+    }
+
+    // Print the result
+    cout << " = " << factorial << endl;
+
+    return 0;
 }
+
